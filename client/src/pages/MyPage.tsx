fc4e71a79033f708f6c0be12f7411ab2d036de9e
MasakiFukunishi <fmsknn@gmail.com>
2023-10-19 14:50:56 -0700

    Add firebase authentification

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,9 +1,14 @@
 import Header from "../components/header";
+import { login, logout } from "../utils/auth";
+import useAuth from "../hooks/useAuth";
+
 const MyPage: React.FC = () => {
+  const user = useAuth();
+  console.log(user);
   return (
     <div className="bg-gray-900 text-gray-50 min-h-screen py-1 px-3">
       <Header />
-      MyPage
+      {user ? <button onClick={logout}>Logout</button> : <button onClick={login}>Login</button>}
     </div>
   );
 };
