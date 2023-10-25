a58e058829a902f4408b1c9cb8596325ba826394
MasakiFukunishi <fmsknn@gmail.com>
2023-10-25 09:52:35 -0700

    Add eslint to server

--- server/src/controllers/tradingController.ts
+++ server/src/controllers/tradingController.ts
@@ -7,11 +7,10 @@ export const addTrading = async (req: Request, res: Response): Promise<void> => 
   const { date, quantity, price, type } = req.body;
   const { user } = req;
   const userId = user?.uid;
-
   try {
     const newTrading = await tradingService.addTrading(userId, date, quantity, price, type);
     res.status(201).json(newTrading);
-  } catch (error: any) {
-    res.status(400).json({ msg: error.message });
+  } catch (error) {
+    console.log(error);
   }
 };
