df2efc0bdf0ed8a359e3154a27a3f7ed361d8278
MasakiFukunishi <fmsknn@gmail.com>
2023-10-26 13:26:37 -0700

    Fix selectbox

---
+++ client/src/components/trading/modals/EditTrading.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+import { close, selectOpenedModal } from "../../../store/slicers/openedModal";
+import { selectConstantTrading } from "../../../store/slicers/constants/trading";
+import InputText from "../../forms/InputText";
+import SelectBox from "../../forms/SelectBox";
+import tradingApi from "../../../api/trading";
+import useAuth from "../../../hooks/useAuth";
+import { useErrorHandling } from "../../../hooks/useErrorHandling";
+
+const EditTrading: React.FC = () => {
+  const user = useAuth();
+  const dispatch = useDispatch();
+  const constantTrading = useSelector(selectConstantTrading);
+  const { param } = useSelector(selectOpenedModal);
+  const [date, setDate] = useState<string>(param.date);
+  const [quantity, setQuantity] = useState<number>(param.quantity);
+  const [price, setPrice] = useState<number>(param.price);
+  const [type, setType] = useState<string>(param.type);
+  const { errors, handleErrors } = useErrorHandling({ date: [], quantity: [], price: [], type: [] });
+
+  const handleInputNumberChange = (e: React.ChangeEvent<HTMLInputElement>, setValue: React.Dispatch<React.SetStateAction<number>>) => {
+    const value = parseInt(e.target.value);
+    if (value >= 0) setValue(value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await tradingApi.add(user.token, { date, quantity, price, type });
+      dispatch(close());
+    } catch (error) {
+      handleErrors(error);
+    }
+  };
+
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-screen flex bg-modal-back justify-center overflow-hidden"
+      onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.target === e.currentTarget && dispatch(close());
+      }}
+    >
+      <div className="w-95% max-w-sm m-auto bg-gray-800 rounded-lg">
+        <div className="flex items-center justify-between h-10 border-b px-3">
+          <div className="font-bold">Edit Trading</div>
+          <FontAwesomeIcon icon={faXmark} size="lg" className="cursor-pointer" onClick={() => dispatch(close())} />
+        </div>
+        <form className="mx-7" onSubmit={handleSubmit}>
+          <div className="mt-5">
+            <InputText
+              id="quantity"
+              label="Quantity"
+              isRequired={true}
+              isNumber={true}
+              state={quantity}
+              errors={errors.quantity}
+              handleChange={(e) => handleInputNumberChange(e, setQuantity)}
+            />
+          </div>
+          <div className="mt-5">
+            <InputText
+              id="price"
+              label="Price"
+              isRequired={true}
+              isNumber={true}
+              state={price}
+              errors={errors.price}
+              handleChange={(e) => handleInputNumberChange(e, setPrice)}
+            />
+          </div>
+          <div className="mt-5">
+            <SelectBox
+              id="type"
+              label="Trading Type"
+              isRequired={true}
+              state={type}
+              options={constantTrading.TRADING_TYPE}
+              errors={errors.type}
+              handleChange={(e) => setType(e.target.value)}
+            />
+          </div>
+          <div className="mt-5">
+            <InputText
+              id="date"
+              label="Date"
+              isRequired={true}
+              state={date}
+              errors={errors.date}
+              handleChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-end h-8 mt-11 mb-5">
+            <button type="button" className="rounded-md border border-gray-50 w-16 mr-2" onClick={() => dispatch(close())}>
+              Cancel
+            </button>
+            <button type="submit" className="bg-blue-600 rounded-md border border-blue-400 w-16">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditTrading;
