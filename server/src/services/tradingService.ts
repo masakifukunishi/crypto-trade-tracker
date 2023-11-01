cbed86b7e6e4a184d0aab9403f33e798bcaec100
MasakiFukunishi <fmsknn@gmail.com>
2023-10-31 21:34:56 -0700

    Add sort

--- server/src/services/tradingService.ts
+++ server/src/services/tradingService.ts
@@ -2,49 +2,11 @@ import TradingModel, { TradingData, TradingDocument } from "../models/tradings.j
 
 class TradingService {
   async getAllTrading(userId: string): Promise<any[]> {
-    const allTrading = await TradingModel.find({ userId });
+    const allTrading = await TradingModel.find({ userId }).sort({ tradeTime: -1 });
     const tradingWithData = allTrading.map((trading) => {
       const totalAmount = trading.price * trading.quantity;
       return { ...trading.toObject(), totalAmount: totalAmount };
     });
-    // return [
-    //   {
-    //     _id: "111",
-    //     userId: "111",
-    //     price: 1,
-    //     quantity: 2,
-    //     date: "1",
-    //     type: 2,
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "222",
-    //     userId: "222",
-    //     price: 2,
-    //     quantity: 1,
-    //     date: "3",
-    //     type: 2,
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "333",
-    //     userId: "333",
-    //     price: 1,
-    //     quantity: 1,
-    //     date: "123131",
-    //     type: 2,
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "444",
-    //     userId: "444",
-    //     price: 1,
-    //     quantity: 1,
-    //     date: "1111111",
-    //     type: 2,
-    //     __v: 0,
-    //   },
-    // ];
     return tradingWithData;
   }
   async addTrading(userId: string, tradeTime: number, quantity: number, price: number, type: number): Promise<TradingDocument> {
