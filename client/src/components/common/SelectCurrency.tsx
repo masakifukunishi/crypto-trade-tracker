c374d3f57ba6b5699e5a79b56dd0a5dd828adc39
MasakiFukunishi <fmsknn@gmail.com>
2023-11-04 11:32:14 -0700

    Change select coin

--- client/src/components/common/SelectCurrency.tsx
+++ client/src/components/common/SelectCurrency.tsx
@@ -3,12 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { selecKrakenConfig } from "../../store/slicers/config";
 import { selectSelectedCoin, setSelectedCoin } from "../../store/slicers/common";
 
+type Props = {
+  isUseAll?: boolean;
+};
+
 interface CurrencyPair {
   displayName: string;
   value: string;
 }
 
-const SelectCurrency = () => {
+const SelectCurrency: React.FC<Props> = ({ isUseAll = false }) => {
   const krakenConfig = useSelector(selecKrakenConfig);
   const quoteAssets = krakenConfig.quoteAssets;
   const currencyOptions = quoteAssets.map((quoteAsset: { symbol: string; altname: string }) => ({
@@ -31,11 +35,14 @@ const SelectCurrency = () => {
         onChange={handleChange}
         className="border font-semibold rounded-lg block w-full px-2 py-1.5 bg-gray-700 border-gray-600 hover:border-blue-500 focus:border-blue-500 cursor-pointer"
       >
-        {currencyOptions.map((pair: CurrencyPair) => (
-          <option key={pair.value} value={pair.value}>
-            {pair.displayName}
-          </option>
-        ))}
+        <>
+          {isUseAll && <option value="">All Coin</option>}
+          {currencyOptions.map((pair: CurrencyPair) => (
+            <option key={pair.value} value={pair.value}>
+              {pair.displayName}
+            </option>
+          ))}
+        </>
       </select>
     </>
   );
