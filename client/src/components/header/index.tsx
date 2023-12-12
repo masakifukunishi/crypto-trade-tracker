7949f8f6e0880eb4be04b2ab35fc7d939d9f7d94
MasakiFukunishi <fmsknn@gmail.com>
2023-12-11 22:53:52 -0800

    Add landing page

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -8,15 +8,15 @@ import { faChartColumn, faUser } from "@fortawesome/free-solid-svg-icons";
 const HeaderComponent: React.FC = () => {
   const location = useLocation();
 
-  const isHomePage = location.pathname === "/";
+  const isChartPage = location.pathname === "/chart";
   const isMyPage = location.pathname === "/my-page";
 
   return (
     <header className="mb-2 pl-1 pt-1.5 flex items-start mt-1 flex-row items-end justify-between">
       <LogoComponent />
       <div className="text-2xl">
-        <LinkItem to="/" icon={faChartColumn} isActive={isHomePage} />
         <LinkItem to="/my-page" icon={faUser} isActive={isMyPage} />
+        <LinkItem to="/chart" icon={faChartColumn} isActive={isChartPage} />
       </div>
     </header>
   );
